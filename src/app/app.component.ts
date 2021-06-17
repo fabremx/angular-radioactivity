1cd81770853a575b12049ba019423f876e3c8ebb
Maxime Fabre <fabre.m96@gmail.com>
2021-06-17 11:55:22 +0200

    Add items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Radioactrivity';
+  items = [
+    { name: 'Paris', zipcode: '75', value: 52 },
+    { name: 'Bordeaux', zipcode: '33', value: 120 },
+    { name: 'Lyon', zipcode: '69', value: 28 },
+    { name: 'Lille', zipcode: '59', value: 251 },
+    { name: 'Marseille', zipcode: '13', value: 78 },
+    { name: 'Nantes', zipcode: '44', value: 102 }
+  ]
 }
